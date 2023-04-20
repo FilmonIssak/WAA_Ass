b03c41b3909b867bf2b7708a36094bccce145516
Filmon <fissak@miu.eduk>
2023-04-20 02:42:12 -0500

    file deleted

--- Lab8/Server/src/main/java/miu/edu/post/Repo/PostRepo.java
+++
@@ -1,9 +0,0 @@
-package miu.edu.post.Repo;
-
-import miu.edu.post.Domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepo extends JpaRepository<Post, Long> {
-}
